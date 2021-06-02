f8791d9d2069ddffc8374628d050b1955bcba117
Sasith-Thanuka <85065089+Sasith-Thanuka@users.noreply.github.com>
2021-06-03 00:24:52 +0530

    Add files via upload

---
+++ MultiHW.java
@@ -0,0 +1,7 @@
+class MultiHW {
+public static void main(String[] args){
+	int x=11,y=4,Multi;
+	Multi =x*y;
+	System.out.println(Multi);
+}
+}
---
+++ SumHW.java
@@ -0,0 +1,7 @@
+class SumHW {
+	public static void main(String[] Args){
+		int x=5,y=8,sum;
+		sum= x+y;
+		System.out.println(sum);
+	}
+}
